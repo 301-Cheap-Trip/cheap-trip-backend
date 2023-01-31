fc66e3381b2f62568a8464e2c47e56eaf8c09f3a
Marcus Bynoe <marcusbynoe57@gmail.com>
2023-01-30 17:11:23 -0700

    minor changes to server

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 'use strict';
 
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-// const { response } = require('express');
 const axios = require('axios')
 
 
 app.use(cors());
 // app.use(express.json());
+mongoose.connect(process.env.DB_URL);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongoose is connected');
+});
+
 
 const PORT = process.env.PORT || 3003;
 
@@ -49,14 +55,16 @@ async function getDirections(request, response, next) {
   
   try {
 
-    let url = `http://api.openweathermap.org/geo/1.0/direct?q=Seattle,WA&limit=1&appid=${process.env.LOCATION_API_KEY}`
+    let url = `http://api.openweathermap.org/geo/1.0/direct?q=Seattle,Washington&limit=1&appid=${process.env.LOCATION_API_KEY}`
 
-    let url2 = `http://api.openweathermap.org/geo/1.0/direct?q=Denver,CO&limit=1&appid=${process.env.LOCATION_API_KEY}`
+    let url2 = `http://api.openweathermap.org/geo/1.0/direct?q=Denver,Colorado&limit=1&appid=${process.env.LOCATION_API_KEY}`
     
     let cityOneData = await axios.get(url);
     let cityTwoData = await axios.get(url2);
+    
 
     let latOne = cityOneData.data[0].lat;
+    console.log(latOne);
     let lonOne = cityOneData.data[0].lon;
 
     
@@ -65,7 +73,7 @@ async function getDirections(request, response, next) {
 
     let directionData = await axios.get(`https://us1.locationiq.com/v1/directions/driving/${latOne},${lonOne};${latTwo},${lonTwo}?key=${process.env.LOCATION_API_KEY_TWO}&geometries=geojson&overview=full`)
 
-    response.status(200).send(cityOneData.data);
+    response.status(200).send(directionData.data);
     
   } catch (error) {
     next(error)
